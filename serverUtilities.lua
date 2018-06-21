1aa578ea1eaf264c41a6138e0212b8521e34579d
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2018-06-21 12:59:07 -0600

    Update serverUtilities.lua

--- serverUtilities.lua
+++ serverUtilities.lua
@@ -1,19 +1,42 @@
 local funcs = {}
 
-funcs.report = function(mdm)
-
+funcs.report = function(mdm,cnl,max,ignore)
+  local ids = {}
+  for i = 1,10 do
+    mdm.transmit(cnl,cnl,"REPORT")
+  end
+  for i = 1,max do
+    local timeOut = os.startTimer(30)
+    while true do
+      local miniTimeOut = os.startTimer(2)
+      local event = {os.pullEvent()}
+      if event[1] == "timer" then
+        if event[2] == timeOut then
+          break
+        end
+      elseif event[1] == "modem_message" then
+        local a,b = event[5]:find("REPORT")
+        if a then
+          mdm.transmit(cnl,cnl,"CONNECT"..event[5]:sub(b+1)..tostring(#ids))
+          print("Connected to "..tostring(#ids))
+          ids[i] = event[5]:sub(b+1)
+          break
+        end
+      end
+    end
+  end
 end
 funcs.allHome = function(mdm)
 
 end
-funcs.allGo = function(mdm)
+funcs.allGo = function(mdm,cnl)
   for i = 1,10 do
-    mdm.transmit(74,74,"ALLGO")
+    mdm.transmit(cnl,cnl,"ALLGO")
   end
 end
-funcs.allStop = function(mdm)
+funcs.allStop = function(mdm,cnl)
   for i = 1,30 do
-    mdm.transmit(74,74,"HALT")
+    mdm.transmit(cnl,cnl,"HALT")
   end
 end
 funcs.tellOneStop = function(mdm,id)
