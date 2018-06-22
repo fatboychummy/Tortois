d377e54aa0bf7c063ac38b715739770dcc194ae1
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2018-06-21 18:21:14 -0600

    Update serverUtilities.lua

--- serverUtilities.lua
+++ serverUtilities.lua
@@ -1,51 +1,7 @@
 local funcs = {}
 
 funcs.report = function(mdm,cnl,max,ignore,monitor)
-  TSLog.connect("Begin connection",2,monitor)
-  local ids = {}
-  for i = 1,max do
-    local timeOut = os.startTimer(30)
-    local connecting = true
-    while connecting do
-      mdm.transmit(cnl,cnl,"REPORT")
-      local miniTimeOut = os.startTimer(2)
-      local event = {os.pullEvent()}
-      if event[1] == "timer" then
-        if event[2] == timeOut then
-          connecting = false
-        end
-      else
-        os.cancelTimer(miniTimeOut)
-      end
-      if event[1] == "modem_message" then
-        local a,b = event[5]:find("REPORT")
-        if a then
-          local rejected = false
-          local cID = event[5]:sub(b+1)
-          for i = 1,#ids do
-            if ids[i] == cID then
-              for i = 1,5 do
-                mdm.transmit(cnl,cnl,"REJECT"..cID)
-                os.sleep()
-              end
-              TSLog.connect("Rejected "..cID,0,monitor)
-              rejected = true
-            end
-          end
-          if not rejected then
-            for i = 1,5 do
-              mdm.transmit(cnl,cnl,"CONNECT"..event[5]:sub(b+1)..tostring(#ids+1))
-              os.sleep()
-            end
-            TSLog.connect("Connected to "..tostring(#ids+1),1,monitor)
-            ids[i] = event[5]:sub(b+1)
-            connecting = false
-          end
-        end
-      end
-    end
-  end
-  return ids
+  TSLog.info("Do nothing fuckers")
 end
 funcs.allHome = function(mdm)
 
